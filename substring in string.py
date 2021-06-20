59b3a366387d3cb70eebb1b499286debff9c983d
Prakash Devraj <85379858+Prakashdevraj781@users.noreply.github.com>
2021-06-21 00:30:54 +0530

    Add files via upload

--- substring in string.py
+++ substring in string.py
@@ -1,15 +1,12 @@
-string='ABCDCDC'
-substring='CDC'
-count=0
-i=0
-while i!=-1:
-    i=string.find(substring,i+1)
-    if(i!=-1):
-        count=count+1
+s='qA2'
+print(s.isalnum())
+for char in s:
+    if any(s)==s.isalpha():
+        print('Ttue')
     else:
-        break
-print(count)
+        print('False')
 
+    
 
 
 
